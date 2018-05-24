105fd2269457bf6770ccec730d6391ddec358d80
Blaszczu <krzysb41@gmail.com>
2018-05-24 23:21:44 +0200

    dodanie quicksorta

--- Lab1/Lab3.py
+++ Lab1/Lab3.py
@@ -3,11 +3,10 @@ import time
 
 
 list5000 = []
-list100 = []
-listprob = [5, 2, 6, 8, 6, 11, 0, 3]
+listprob = [5, 2, 6, 8, 7, 11, 0, 3,9]
 for i in range(5000):
     list5000.append(random.randrange(0,100))
-listprzyklad = [2,4,1,3]
+
 
 
 def bubblesort(list):
@@ -29,36 +28,47 @@ def bubblesort(list):
 def selectionsort(list):
     tosort = list.copy()
     tosortlength = len(tosort)
-    sorted = False
-    while not sorted:
-        sorted = True
-        for i in range(tosortlength-1):
-            minindex = i
-            for j in range(len(tosort[i:])):
-                if tosort[i+j:i+j+1].pop() < tosort[minindex]:
-                    minindex = j+i
-            temp = tosort[i]
-            tosort[i]= tosort[minindex]
-            tosort[minindex]=temp
-            #print(minindex, "  LICZBA w liscie ", tosort[minindex], "   ", tosort)
+    for i in range(tosortlength-1):
+        minindex = i
+        for j in range(len(tosort[i:])):
+            if tosort[i+j:i+j+1].pop() < tosort[minindex]:
+                minindex = j+i
+        temp = tosort[i]
+        tosort[i]= tosort[minindex]
+        tosort[minindex]=temp
     return tosort
 
 
 def quicksort(list):
+    if not list:
+        return []
     tosort = list.copy()
     tosortlength = len(tosort)
-    return tosort
+    index=int(tosortlength/2)
+    pivot = tosort[index]
+    lower = []
+    higher = []
+    pivots = []
+    for i in range(tosortlength):
+        if tosort[i]<pivot:
+            lower.append(tosort[i])
+        elif tosort[i]>pivot:
+            higher.append(tosort[i])
+        else:
+            pivots.append(tosort[index])
+    return quicksort(lower) + pivots + quicksort(higher)
+
 
 
 if __name__ == '__main__':
     timebubble =time.time()
-    print(bubblesort(list5000))
+    bubblesort(list5000)
     print("BubbleSort time: ", time.time()-timebubble)
 
     timeselection = time.time()
-    print(selectionsort(list5000))
+    selectionsort(list5000)
     print("SelectionSort time: ", time.time() - timeselection)
 
     timequick = time.time()
-    print(quicksort(list5000))
+    quicksort(list5000)
     print("QuickSort time: ", time.time() - timequick)
